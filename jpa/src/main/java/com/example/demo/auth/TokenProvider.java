b71566626ce19e06c5880823f1a7423504449661
ChaeyoonN <codbs0703@naver.com>
2023-12-13 19:04:01 +0900

    게시판/등록까지 완료/231213

--- jpa/src/main/java/com/example/demo/auth/TokenProvider.java
+++ jpa/src/main/java/com/example/demo/auth/TokenProvider.java
@@ -1,15 +1,17 @@
 package com.example.demo.auth;
 
 import com.example.demo.userapi.entity.User;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import io.jsonwebtoken.security.WeakKeyException;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
@@ -21,12 +23,12 @@ import java.util.Map;
 // 역할: 토큰을 발급하고, 서명 위조를 검사하는 객체.
 public class TokenProvider {
 
-
-
     // 서명에 사용할 값 (512비트 이상의 랜덤 문자열)
     // @Value: properties 형태의 파일의 내용을 읽어서 변수에 대입하는 아노테이션. (yml도 가능)
     @Value("${jwt.secret}")
     private String SECRET_KEY;
+
+
 
     // 토큰 생성 메서드
     /**
@@ -38,7 +40,7 @@ public class TokenProvider {
 
         // 토큰 만료 시간 생성
         Date expiry = Date.from(
-                Instant.now().plus(1, ChronoUnit.HOURS)
+                Instant.now().plus(15, ChronoUnit.MINUTES)
         );
 
         // 토큰 생성
@@ -72,7 +74,34 @@ public class TokenProvider {
                 .setExpiration(expiry) // exp: 만료 시간
                 .setSubject(userEntity.getUserId()) // sub: 토큰을 식별할 수 있는 주요 데이터
                 .compact(); // String 리턴.
+
+
     }
+
+
+    // 토큰 위조 여부 확인
+    public TokenUserInfo validateAndGetTokenUserInfo(String token) {
+        // 사용자가 로그인 후 어떠한 요청을 할때 토큰값이 브라우저에서 같이 넘어오면
+        // 그 토큰 값이 위조가 되었는지를 검사하기위해서 토큰 -> 객체 형태로
+        // 풀어주는곳
+        Claims claims = Jwts.parserBuilder()
+                    // 토큰 발급자의 발급 당시의 서명을 넣어줌
+                    .setSigningKey(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()))
+                    // 서명 위조 검사: 위조된 경우에는 예외가 발생합니다.
+                    // 위조가 되지 않은 경우 payload를 리턴
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+
+            log.info("claims: {}", claims);
+
+        return TokenUserInfo.builder()
+                .userId(claims.getSubject())
+                .email(claims.get("email", String.class))
+                .build(); // claims 중에 쓰고 싶은 것만 뽑아 TokenUserInfo 객체로 포장
+
+    }
+
 
 
 
--- jpa/src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ jpa/src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -1,15 +1,22 @@
 package com.example.demo.config;
 
+import com.example.demo.filter.JwtAuthFilter;
+import com.example.demo.filter.JwtExcetionFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.CorsFilter;
 
 @EnableWebSecurity // 시큐리티 설정 파일로 사용할 클래스 선언
 @RequiredArgsConstructor
 public class WebSecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+    private final JwtExcetionFilter jwtExcetionFilter;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         // Security 모듈이 기본적으로 제공하는 보안 정책 해제
@@ -24,6 +31,17 @@ public class WebSecurityConfig {
                 .and()
                 .authorizeRequests()
                 .anyRequest().permitAll();
+
+
+        // 토큰 인증 필터 연결
+        // jwtAuthFilter부터 연결 -> CORS 필터를 이후에 통과하도록 설정.
+        http.addFilterAfter(
+                jwtAuthFilter, //  CorsFilter 전에 필터 써주기!
+                CorsFilter.class // import 주의: 스프링 꺼로
+        );
+
+        // Exception Filter를 Auth Filter 앞에 배치를 하겠다는 뜻.
+        http.addFilterBefore(jwtExcetionFilter, JwtAuthFilter.class);
 
         return http.build();
     }
