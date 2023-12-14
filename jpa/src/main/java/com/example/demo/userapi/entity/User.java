34978f5c6b30c905214c0d7a9c42ff280a923cc4
hansol na <pinecone171@gmail.com>
2023-12-14 17:49:41 +0900

    유저 주소 추가/231214

--- jpa/src/main/java/com/example/demo/userapi/entity/User.java
+++ jpa/src/main/java/com/example/demo/userapi/entity/User.java
@@ -33,14 +33,15 @@ public class User {
     @Column(nullable = false)
     private String userName;
 
+    private String userAddress;
+
     @CreationTimestamp
     private LocalDateTime joinDate;
 
-    private String accessToken;
+    @OneToOne(cascade = CascadeType.ALL)
+    private SnsLogin snsLogin;
 
-    public void setAccessToken(String accessToken){
-        this.accessToken = accessToken;
-    }
+    public void setSnsLogin(SnsLogin snsLogin) {this.snsLogin = snsLogin;}
 }
 
 
---
+++ jpa/src/main/java/com/example/demo/userapi/repository/SnsLoginRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.userapi.repository;
+
+import com.example.demo.userapi.entity.SnsLogin;
+import com.example.demo.userapi.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SnsLoginRepository
+        extends JpaRepository<SnsLogin, String> {
+}
