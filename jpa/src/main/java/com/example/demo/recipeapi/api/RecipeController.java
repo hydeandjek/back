05d78cc75c31078b311b1848c9102d9398c2f6a1
ChaeyoonN <codbs0703@naver.com>
2023-12-20 17:13:20 +0900

    레시피api/완료(예외처리 진행중)231220

--- jpa/src/main/java/com/example/demo/recipeapi/api/RecipeController.java
+++ jpa/src/main/java/com/example/demo/recipeapi/api/RecipeController.java
@@ -4,8 +4,12 @@ import com.example.demo.recipeapi.dto.recipeListResponseDTO;
 import com.example.demo.recipeapi.service.RecipeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 
 @RestController
 @Slf4j
@@ -27,23 +31,29 @@ public class RecipeController {
     }
 
     // 카테고리별 레시피 리스트 요청 처리
-    @GetMapping("/{category}")
-    public ResponseEntity<?> getRecipeList(@PathVariable String category){
-        log.info("/api/recipe/{} - GET Recipe List Request", category);
-//        String responseDTO = recipeService.getRecipeList();
+    @GetMapping("/{category}/{pageNum}") // 예: 반찬/1페이지
+    public ResponseEntity<?> getRecipeList(@PathVariable String category, @PathVariable int pageNum) {
+        log.info("/api/menu/recipe/{}/{} - GET Recipe List by Category Request", category, pageNum);
+        String responseDTO = recipeService.getRecipeListByCategory(category, pageNum);
 
-//        return ResponseEntity.ok().body(responseDTO);
-        return null;
+        return ResponseEntity.ok().body(responseDTO);
     }
 
     // 레시피 상세보기 요청 처리
-    @GetMapping("/{category}/{id}")
+    @GetMapping("detail/{category}/{id}")
     public ResponseEntity<?> getRecipe(@PathVariable String category, @PathVariable String id){
-        log.info("/api/recipe/{}/{} - GET Recipe Detail Request", category, id);
-        String responseDTO = recipeService.getRecipe(id);
+        try {
+//            URLEncoder.encode(category, "UTF-8");
+            log.info("/api/menu/recipe/detail/{}/{} - GET Recipe Detail Request"
+                    , category, id);
+            String responseDTO = recipeService.getRecipe(category, id);
 
 
-        return ResponseEntity.ok().body(responseDTO);
+            return ResponseEntity.ok().body(responseDTO);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
 
     }
 
--- jpa/src/main/java/com/example/demo/recipeapi/service/RecipeService.java
+++ jpa/src/main/java/com/example/demo/recipeapi/service/RecipeService.java
@@ -1,19 +1,22 @@
 package com.example.demo.recipeapi.service;
 
-import com.example.demo.recipeapi.entity.QRecipe;
-import com.example.demo.recipeapi.entity.Recipe;
+
 import com.example.demo.recipeapi.repository.RecipeRepository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.beans.factory.annotation.Value;
 
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
-import java.net.HttpURLConnection;
+
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -37,13 +40,18 @@ public class RecipeService {
 
     public String getRecipeList(int pageNum) {
 //        factory = new JPAQueryFactory(em);
-//
 //        List<Recipe> recipeList = factory.selectFrom()
+        String reqUri;
+        if(pageNum == 1){ // 1
+            reqUri = "http://openapi.foodsafetykorea.go.kr/api/"+getApiKey()+"/COOKRCP01/json/"
+                    +pageNum+"/"+ pageNum*6;
+        }else { // 2~
+            reqUri = "http://openapi.foodsafetykorea.go.kr/api/"+getApiKey()+"/COOKRCP01/json/"
+                    +(((pageNum-1)*6)+1)+"/"+ pageNum*6;
+        }
 
 
 
-        String reqUri = "http://openapi.foodsafetykorea.go.kr/api/"+getApiKey()+"/COOKRCP01/json/"
-        +pageNum+"/"+ pageNum*6;
 //        HttpURLConnection urlConnection = (HttpURLConnection) reqUri.open
 
         // 요청 헤더 설정?
@@ -76,7 +84,36 @@ public class RecipeService {
     }
 
 
-    public String getRecipe(String id) {
+    public String getRecipeListByCategory(String category, int pageNum) {
+        String reqUri;
+        if(pageNum == 1){ // 1
+            reqUri = "http://openapi.foodsafetykorea.go.kr/api/"+getApiKey()+"/COOKRCP01/json/"
+                    +pageNum+"/"+ pageNum*6+"/RCP_PAT2="+category;
+        }else { // 2~
+            reqUri = "http://openapi.foodsafetykorea.go.kr/api/"+getApiKey()+"/COOKRCP01/json/"
+                    +(((pageNum-1)*6)+1)+"/"+ pageNum*6+"/RCP_PAT2="+category;
+        }
+
+        // 요청 보내기
+        RestTemplate template = new RestTemplate();
+
+        // 통신을 보내면서 응답데이터를 리턴
+        // param1: 요청 url
+        // param2: 요청 메서드
+        // param3: 헤더와 요청파라미터정보 엔터티
+        // param4: 응답데이터를 받을 객체의 타입 (ex: dto, map)
+//        ResponseEntity<KakaoUserDTO> responseEntity
+//                = template.exchange(reqUri, HttpMethod.GET, new HttpEntity<>(headers), recipeListResponseDTO.class);
+
+        // 응답 데이터에서 필요한 정보를 가져오기
+        Map<String, Objects> responseBody = template.getForObject(reqUri, Map.class);
+        log.info("레시피 리스트 요청 응답 데이터: {}", responseBody.toString());
+
+        return responseBody.toString();
+    }
+
+    public String getRecipe(String category, String id) throws ParseException {
+
         String reqUri = "http://openapi.foodsafetykorea.go.kr/api/"+getApiKey()+"/COOKRCP01/json/"+id+"/"+id;
 
         RestTemplate template = new RestTemplate();
@@ -84,8 +121,30 @@ public class RecipeService {
         Map<String, Objects> responseBody = template.getForObject(reqUri, Map.class);
         log.info("레시피 리스트 요청 응답 데이터: {}", responseBody.toString());
 
+        // api요청으로 받아온 json데이터의 요리 카테고리 추출
+//        JSONParser jsonParser = new JSONParser();
+//        //JSON데이터를 넣어 JSON Object 로 만들어 준다.
+//         JSONObject jsonObject = (JSONObject) jsonParser.parse(responseBody
+//                                                                .toString()
+//                                                                .replaceAll("\\uFEFF", ""));
+//
+//        JSONObject cookrcp01 = jsonObject.get("COOKRCP01"); // 배열 추출
+//        JSONArray recipe = (JSONArray) jsonObject.get("row"); // 배열 추출
+//        JSONArray recipe = (JSONArray) jsonObject.get("row"); // 배열 추출
+//        log.info("배열: {}", recipe);
+//
+//         JSONObject recipeObj = (JSONObject) recipe.get(0); // 배열 안 객체 추출
+//        Object rcpCategory = recipeObj.get("RCP_PAT2"); // 객체 안의 키의 값 추출
+//
+//        // 매개변수로 받은 카테고리 != api요청에 따라 받은 요리의 카테고리
+//        if(!category.equals(rcpCategory)){
+//            throw new IllegalArgumentException("해당 카테고리와 아이디에 맞는 레시피를 가져올 수 없습니다!");
+//        }
         return responseBody.toString();
 
 
     }
+
+
+
 }
