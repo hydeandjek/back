d02ec16d3ac9fb913c938f49ca9ee53545f9cb22
hansol na <pinecone171@gmail.com>
2023-12-28 13:57:55 +0900

    지원기관 API 추가 / 231228

--- jpa/src/main/java/com/example/demo/publicapi/service/PublicService.java
+++ jpa/src/main/java/com/example/demo/publicapi/service/PublicService.java
@@ -1,12 +1,16 @@
 package com.example.demo.publicapi.service;
 
+import com.example.demo.publicapi.dto.PublicResponseDTO;
 import com.example.demo.publicapi.repository.PublicRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -16,7 +20,21 @@ public class PublicService {
 
     private final PublicRepository publicRepository;
 
+    public List<PublicResponseDTO> getPublics(int pageNum) {
+        PageRequest pageRequest = PageRequest.of(pageNum - 1, 10);
+        return publicRepository.findAll(pageRequest).get().map(item -> PublicResponseDTO.builder()
+                .data_id(item.getData_id())
+                .publicTitle(item.getPublicTitle())
+                .publicNum(item.getPublicNum())
+                .publicAddr(item.getPublicAddr())
+                .x(item.getX())
+                .y(item.getY())
+                .build())
+                .collect(Collectors.toList());
+    }
 
 
-
+    public long getTotalCount() {
+        return publicRepository.count();
+    }
 }
